e830ad5fc3fe3345a0b8857f3c3a0ad0034098ae
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-12-17 14:08:02 +0300

    add hashing

---
+++ hashing.php
@@ -0,0 +1,9 @@
+<?php
+    $password = "pizza123";
+    $hash = password_hash($password,PASSWORD_DEFAULT);
+
+
+    if(password_verify("pizza123", $hash)){
+        echo "You are logged in";
+    }
+?>
